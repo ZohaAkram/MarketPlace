7e4a9e7ab75987203c97431a6bf6948759702f50
Zoha Akram <zoha.ak24@gmail.com>
2021-10-01 20:40:37 +0500

    create book product

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -3,7 +3,33 @@ pragma solidity >= 0.5.8;
 
 contract Marketplace{
 string public name;
+uint public prodCount=0;
+mapping(uint=>BookProduct) public bookProducts;
 
- constructor()public {
+
+struct BookProduct{
+    uint id;
+    string name;
+    uint price;
+    address owner;
+    bool purchased;
+}
+
+event BookProdCreated(
+uint id,
+    string name,
+    uint price,
+    address owner,
+    bool purchased
+);
+constructor()public {
 name="Online Book Marketplace"; }
+
+function createBookProd(string memory _name,uint _price) public{
+require(bytes(name).length>0); //checks for empty name
+require(_price> 0); //checks for price
+prodCount++;
+bookProducts[prodCount]=BookProduct(prodCount,_name,_price,msg.sender,false);
+emit BookProdCreated(prodCount, _name, _price, msg.sender, false);
+}
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,7 +1,7 @@
 const { assert } = require("chai");
 
 const Marketplace=artifacts.require("./Marketplace.sol");
-contract('Marketplace',(account)=>{
+contract('Marketplace',([deployer,seller,buyer])=>{
     let marketplace
     before(async()=>{
         marketplace=await Marketplace.deployed()
@@ -19,5 +19,24 @@ contract('Marketplace',(account)=>{
             const name=await marketplace.name()
             assert.equal(name,'Online Book Marketplace')
         })
+    })
+
+    describe('products',async()=>{
+        let result,prodCount
+        before(async()=>{
+            result=await marketplace.createBookProd('Intro to Programming',web3.utils.toWei('1','Ether'),{from:seller})
+           prodCount=await marketplace.prodCount()
+        })
+
+        it('creates Products',async()=>{
+            // success
+           assert.equal(prodCount,1); //checks for product count
+       const event= result.logs[0].args;
+       assert.equal(event.id.toNumber(),prodCount.toNumber(),'id is correct')
+       assert.equal(event.name,'Intro to Programming','name is correct')
+       assert.equal(event.price, 1000000000000000000,'price is correct')
+       assert.equal(event.owner,seller ,'owner is correct')
+       assert.equal(event.purchased, false,'purchased is correct')   
+               })
     })
 })
