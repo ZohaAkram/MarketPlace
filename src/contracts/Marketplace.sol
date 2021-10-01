b168d3994fc954237b3c9ddcdfe0fa2711089a1a
Zoha Akram <zoha.ak24@gmail.com>
2021-10-01 19:48:50 +0500

    setting env

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.8;
+
+contract Marketplace{
+string public name;
+
+ constructor()public {
+name="Online Book Marketplace"; }
+}
---
+++ test/Marketplace.test.js
@@ -0,0 +1,23 @@
+const { assert } = require("chai");
+
+const Marketplace=artifacts.require("./Marketplace.sol");
+contract('Marketplace',(account)=>{
+    let marketplace
+    before(async()=>{
+        marketplace=await Marketplace.deployed()
+    })
+    describe('deployment',async()=>{
+        it('deploys successfully',async()=>{
+            const address = await marketplace.address
+            assert.notEqual(address,0x0)
+            assert.notEqual(address,'')
+            assert.notEqual(address,null)
+            assert.notEqual(address,undefined)
+  
+        })
+        it('has a name',async()=>{
+            const name=await marketplace.name()
+            assert.equal(name,'Online Book Marketplace')
+        })
+    })
+})
